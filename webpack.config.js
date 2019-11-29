5ece011d0aa3dc1b09b2fd763e00344172019a83
Hare Ram Rai <hareram.rai@bigbinary.com>
2019-11-29 22:11:02 +0530

    Initial commit for battle application

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./app/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "index_bundle.js"
+    filename: "index_bundle.js",
+    publicPath: "/"
   },
   module: {
     rules: [
@@ -13,10 +15,14 @@ module.exports = {
       { test: /\.css$/, use: ["style-loader", "css-loader"] }
     ]
   },
-  mode: "development",
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
   plugins: [
     new HtmlWebpackPlugin({
       template: "app/index.html"
-    })
-  ]
+    }),
+    new CopyPlugin([{ from: "_redirects" }])
+  ],
+  devServer: {
+    historyApiFallback: true
+  }
 };
